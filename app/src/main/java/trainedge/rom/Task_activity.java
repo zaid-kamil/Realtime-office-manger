3b23eb1d111438b0911e43268ae0d81b26c8b8d8
zaid bin kamil <xaidmetamorphos@gmail.com>
2017-04-29 20:40:29 +0530

    accha

--- app/src/main/java/trainedge/rom/Task_activity.java
+++ app/src/main/java/trainedge/rom/Task_activity.java
@@ -1,21 +1,75 @@
 package trainedge.rom;
 
+import android.database.DataSetObserver;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
-public class Task_activity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Task_activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    public static final String TAG = "TaskActivity";
     EditText tv_info;
     Spinner spin_list;
-    Button  btn_send;
+    Button btn_send;
+    private FirebaseDatabase fb;
+    private DatabaseReference myRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_activity);
-        tv_info= (EditText) findViewById(R.id.tv_info);
-         spin_list= (Spinner) findViewById(R.id.spin_list);
-        btn_send= (Button) findViewById(R.id.btn_send);
+        tv_info = (EditText) findViewById(R.id.tv_info);
+        spin_list = (Spinner) findViewById(R.id.spin_list);
+        btn_send = (Button) findViewById(R.id.btn_send);
+        fb = FirebaseDatabase.getInstance();
+        myRef = fb.getReference("users");
+
+
+       /* myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is :" + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value :", databaseError.toException());
+            }
+        });*/
+        spin_list.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spin_list.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
+
+
